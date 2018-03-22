def9b18a22c0659ee60faccc5b34e9f838274bb9
Marat Genatulin <marat@genatulin.com>
2018-03-22 20:52:59 +0300

    Домашнее задание на проверку преподавателю.

--- Netology_Task_9.py
+++ Netology_Task_9.py
@@ -1,34 +1,74 @@
-import csv
+"""Программа расчета необходимых для готовки блюд ингридиентов."""
 
-flats_list = list()
 
-with open('output.csv', newline='') as csvfile:
-    flats_csv = csv.reader(csvfile, delimiter=';')
-    flats_list = list(flats_csv)
+def return_our_shop_list(our_cook_book, our_dishes, our_person_count):
+    """Расчет необходимых для покупки ингридиентов."""
+    our_list = {}
+    for dish in our_dishes:
+        for ingridient in our_cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
+            new_shop_list_item['quantity'] *= our_person_count
+            if new_shop_list_item['ingridient_name'] not in our_list:
+                our_list[new_shop_list_item['ingridient_name']] \
+                    = new_shop_list_item
+            else:
+                our_list[new_shop_list_item['ingridient_name']]['quantity'] \
+                    += new_shop_list_item['quantity']
+    return our_list
 
-# TODO 1: печать списка новостроек с их порядковыми номерами
-new_flats = list()
-for i, flat in enumerate(flats_list):
-    if 'новостройка' in flat:
-        new_flats.append(flat)
-        print("Порядковый номер {}:  {}".format(i + 1, flat))
 
-# # TODO2: Сравнение 3-х новостроек друг с другом
-count_new_flats = 0
-how_much = 3
-len_new_flats = len(new_flats) - how_much
+def return_cook_book():
+    """Процедура подготовки книги рецептов."""
+    cook_book = {}
+    with open('cook_book.txt', encoding='utf8') as our_file:
 
-while count_new_flats < len_new_flats:
-    flats_intersection = set(new_flats[count_new_flats]) & set(
-        set(new_flats[count_new_flats + 1]) & set(new_flats[count_new_flats + 2]))
-    count_new_flats += 1
-    print('Сравнение новостроек {}, {}, {}:'.format(count_new_flats, count_new_flats + 1, count_new_flats + 2),
-          flats_intersection)
+        finish_reciept = True
 
-# # TODO3: работа со словарем
-test_dict = dict()
-for i, flat in enumerate(flats_list):
-    if i == 0:
-        continue
-    test_dict[flat[len(flat) - 1]] = flat[0]
-print(test_dict)
+        for line in our_file:
+            line = line.strip().lower()
+            if finish_reciept:
+                dish = line
+                dish_reciept = []
+                finish_reciept = False
+                # Строка с количеством ингридиентов
+                #  не требуется, поэтому ее пропускаем.
+                our_file.readline()
+            else:
+                if line == '':
+                    cook_book[dish] = dish_reciept
+                    finish_reciept = True
+                else:
+                    our_ingridient = line.split('|')
+
+                    dish_ingridients = {}
+                    dish_ingridients['ingridient_name'] = our_ingridient[0]
+                    dish_ingridients['quantity'] = float(our_ingridient[1])
+                    dish_ingridients['measure'] = our_ingridient[2]
+                    dish_reciept.append(dish_ingridients)
+        cook_book[dish] = dish_reciept
+
+    return cook_book
+
+
+def return_dishes():
+    """Запрашиваем наименование блюд."""
+    return input(
+        'Введите блюда в расчете на одного человека через запятую: ').\
+        lower().split(', ')
+
+
+def print_shop_list(p_list):
+    """Выводим содержимое листа покупок."""
+    for p_list_item in p_list.values():
+        print('{ingridient_name} {quantity} {measure}'.format(**p_list_item))
+
+
+def main():
+    """Основная процедура."""
+    person_count = int(input('Введите количество человек: '))
+    dishes = return_dishes()
+    cook_book = return_cook_book()
+    print_shop_list(return_our_shop_list(cook_book, dishes, person_count))
+
+
+main()
